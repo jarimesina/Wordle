b34d4c339de0e42c46d7bba22f8eb42f05a6ee6a
Jari Mesina <jarimesina1234@gmail.com>
2022-03-30 19:06:12 +0800

    add and error animation

--- components/Tile/index.tsx
+++ components/Tile/index.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 import styles from './index.module.scss';
-// import { motion, ResolvedValues, transform } from "framer-motion"
 
 export type State = 'default' | 'correct' | 'absent' | 'present';
 
@@ -8,18 +7,29 @@ interface Props {
   state?: State;
   character?: string | void;
   isRevealing?: boolean;
+  isShake: boolean;
   row: number;
   [otherProp: string]: any;
+  // rowClass: string;
 }
 
-const Tile = ({ character = '', row, isRevealing, index, guessCount, state = 'default', ...otherProps }: Props) => {
+const Tile = ({ rowClass, character = '', row, isRevealing, isShake, index, guessCount, state = 'default', ...otherProps }: Props) => {
+  const position = index-(row*5);
 
-  const position = index-((row)*5);
   return (
-    // TODO: add state for tiles as a row if active, passed, not yet passed
-    // <div className={clsx(styles.tile, isRevealing && styles.mod__tileReveal, styles[`mod__${state}`])} style={{ animationDelay }} {...otherProps}>
-    <div className={clsx(styles.tile, styles[`mod__${state}`], isRevealing && styles.mod__tileReveal, styles[`${state}`])} style={{ animationDelay: `${position * 350}ms` }} {...otherProps}>
-      {character}
+    <div className={clsx(
+        styles.tile,
+        styles[`mod__${state}`],
+        isRevealing && styles.mod__tileReveal,
+        styles[`${state}`],
+        isShake ? styles.mod__tileShake : character && styles.mod__characterAdd
+      )}
+      style={{ animationDelay: isRevealing ? `${position * 350}ms`: '0ms' }} 
+      {...otherProps}
+    >
+      <div style={{ animationDelay: isRevealing ? `${position * 350}ms`: '0ms' }}>
+        {character}
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
-import { useAnimation } from 'framer-motion'
 import type { NextPage } from 'next'
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 // import Head from 'next/head'
 // import Image from 'next/image'
-import Key from '../components/Key'
 import Keyboard from '../components/Keyboard'
 import { State } from '../components/Tile'
 import TileBoard from '../components/TileBoard'
-import styles from '../styles/Home.module.css'
+import { WORDS } from '../constants/wordsList'
+import styles from './index.module.scss';
 
 export const answer = "story";
 
@@ -16,16 +15,21 @@ const Home: NextPage = () => {
   // possible idea: setGuessedWords would be an array of objects { character: 'A', status: 'correct' }
   // idea is to store words in a big array or in one string e.g. ['s','t','y','l','e','p','a','p','e','r','o','t','t','e','r']
   // const [guessedWords, setGuessedWords] = useState<string[]>([]);
-  const [guessedWords, setGuessedWords] = useState<{character: string; state: State; isRevealing: boolean}[]>([]);
+  const [guessedWords, setGuessedWords] = useState<{character: string; state: State; isRevealing: boolean; isShake: boolean;}[]>([]);
   const [word, setWord] = useState<string>('');
   const [pressCount, setPressCount] = useState(0);
   const [guessCount, setGuessCount] = useState(0);
+  const [isShake, setIsShake] = useState(false);
+
+  const clearIsShake = () => {
+    setIsShake(false);
+  };
 
   const handleKeyPress = (character: string) => {
     if(pressCount<=4){
       setWord(prev => prev + character);
       // setGuessedWords(prev => prev + character);
-      setGuessedWords(prev => [...prev, { character: character, state: 'default', isRevealing: false}]);
+      setGuessedWords(prev => [...prev, { character: character, state: 'default', isRevealing: false, isShake: false}]);
       setPressCount(prev => prev + 1);
 
       return;
@@ -47,10 +51,19 @@ const Home: NextPage = () => {
   const handleEnter = () => {
     if(guessCount===5) return;
     // TODO: if guess count is 6 then user can't do anything and must press another button to restart the game
-    // every time the user presses enter, count the number of guesses
 
     // setGuessedWords(prev => [...prev, word]);
-    flipKeys();
+    if(!WORDS.includes(word.toLowerCase()) || word.length < 5){
+      setIsShake(true);
+      alert('wrong word');
+      setTimeout(() => {
+        clearIsShake();
+      }, 250)
+
+      return;
+    }
+    flipTiles();
+
     setGuessCount(prev => prev + 1);
     if(answer !== word.toLowerCase()){
       setPressCount(0);
@@ -59,7 +72,9 @@ const Home: NextPage = () => {
     }
 
     // TODO: add kanye west quotes from api
-    // TODO: create component for guessing the correct answer
+    // TODO: create alerts
+    // TODO: make keys accept color change
+    // TODO: add keyboard interaction
     alert("you guessed the answer!");
   }
 
@@ -91,29 +106,21 @@ const Home: NextPage = () => {
     return 'correct';
   }
 
-  // two problems: 
-  // 1. it does not animate in a staggered manner
-  // 2. the flip should happen first before adding the color
-  // solution: index - guesscount times 5 will be the delay value
-
   // this method will set the status on the active row which will cause the tile to flip and change color
-  const flipKeys = useCallback(() => {
+  const flipTiles = useCallback(() => {
     for (let i = 0; i < 5; i++) {
       guessedWords[guessCount*5 + i].isRevealing = true;
-      // console.log(guessedWords[guessCount*5 + i].isRevealing);
-      // guessedWords[guessCount*5 + i].animate = "flip"
-      // solution for the color problem: set a property on update so that when the animation happens, we set the state on the tile
       guessedWords[guessCount*5 + i].state = determineStatus(guessedWords[guessCount*5 + i].character.toLowerCase(), guessCount*5 + i);
     }
   },[guessedWords]);
 
   return (
-    <>
+    <div className={styles.home}>
       <div style={{ width: 299}}>
-        <TileBoard guessCount={guessCount} words={guessedWords} answer={answer} word={word}/>
+        <TileBoard isShake={isShake} guessCount={guessCount} words={guessedWords} answer={answer} word={word}/>
       </div>
       <Keyboard handleEnter={handleEnter} handleKeyPress={handleKeyPress} handleDelete={handleDelete}/>
-    </>
+    </div>
   );
 }
 
